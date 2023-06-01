5117d6b971832b6687c704e1c63a1ea8146a7b67
Pratibha-98100 <130212205+Pratibha-98100@users.noreply.github.com>
2023-06-01 13:08:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,44 +1,22 @@
-const digitToLetters = {
-  2: "abc",
-  3: "def",
-  4: "ghi",
-  5: "jkl",
-  6: "mno",
-  7: "pqrs",
-  8: "tuv",
-  9: "wxyz",
-  0: "0",
-  1: "1",
+var letterCombinations = function(digits) {
+    const result = [];
+    if (!digits) {
+        return result;
+    }
+    const mapping = ["", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"];
+    const combination = new Array(digits.length).fill('');
+    backtrack(result, mapping, combination, digits, 0);
+    return result;
 };
 
-function generateCombinations(event) {
-  event.preventDefault();
-
-  const digits = document.getElementById("digits").value;
-
-  const combinations = letterCombinations(digits);
-  document.getElementById("result").textContent =
-    "Combinations: " + combinations.join(", ");
-}
-
-function letterCombinations(digits) {
-  if (digits.length === 0) {
-    return [];
-  }
-
-  const combinations = [""];
-
-  for (let i = 0; i < digits.length; i++) {
-    const letters = digitToLetters[digits[i]];
-
-    while (combinations[0].length === i) {
-      const currentCombination = combinations.shift();
-
-      for (let j = 0; j < letters.length; j++) {
-        combinations.push(currentCombination + letters[j]);
-      }
+function backtrack(result, mapping, combination, digits, index) {
+    if (index === digits.length) {
+        result.push(combination.join(''));
+    } else {
+        const letters = mapping[digits.charAt(index) - '0'];
+        for (const letter of letters) {
+            combination[index] = letter;
+            backtrack(result, mapping, combination, digits, index + 1);
+        }
     }
-  }
-
-  return combinations;
 }
